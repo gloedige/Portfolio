6c3ede63b1993a8f5bfff508f522456b8d1c9f62
gloedige <gerd.loedige@posteo.de>
2026-03-17 16:42:24 +0100

    create function typeEffect

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+const data = [
+    {icon: './assets/icons/location_icon.png', text_1: 'I am', text_2: 'located in Brunswick'},
+    {icon: './assets/icons/remote_icon.svg', text_1: 'I am', text_2: 'open to work remotely or on-site'},
+    {icon: './assets/icons/relocate_icon.svg', text_1: 'I am', text_2: 'open to relocate'}
+]
+
+
+const textElement1 = document.getElementById('typing_text_part_1');
+const textElement2 = document.getElementById('typing_text_part_2');
+const locationIcon = document.getElementById('location_icon');
+
+let index = 0;
+let isDeleting = false;
+let currentText = '';
+let currentSpanText = '';
+
+function typeEffect() {
+    const fullText = ' ' + data[index].text_2;
+    const fullSpanText = data[index].text_1;
+    const iconSrc = data[index].icon;
+    if (isDeleting) {
+        if (currentText.length > 0) {
+            currentText = fullText.substring(0, currentText.length - 1);
+        } else if (currentSpanText.length > 0) {
+            currentSpanText = fullSpanText.substring(0, currentSpanText.length - 1);
+        }
+    } else {
+        locationIcon.innerHTML = `<img src="${iconSrc}" alt="Location Icon">`;
+        locationIcon.style.opacity = 1;
+        currentSpanText = fullSpanText.substring(0, currentSpanText.length + 1);
+        if (currentSpanText === fullSpanText) {
+            currentText = fullText.substring(0, currentText.length + 1);
+        }
+    }
+    
+    let spanElement = `<span class="text_blue" id="typing_text_part_1">${currentSpanText}</span>`;
+    textElement2.innerHTML = spanElement + currentText;
+
+    let speed = isDeleting ? 50 : 100;
+
+    if (!isDeleting && currentText === fullText) {
+        speed = 2000;
+        isDeleting = true;
+    } else if (isDeleting && currentText === '' && currentSpanText === '') {
+        isDeleting = false;
+        locationIcon.style.opacity = 0;
+        index = (index + 1) % data.length;
+        speed = 500;
+    }
+
+    setTimeout(typeEffect, speed);
+}
+
 function intersectionObserver() {
     const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
@@ -15,4 +68,5 @@ document.querySelectorAll('section').forEach(section => observer.observe(section
 
 document.addEventListener('DOMContentLoaded', () => {
     intersectionObserver();
+    typeEffect();
 });
